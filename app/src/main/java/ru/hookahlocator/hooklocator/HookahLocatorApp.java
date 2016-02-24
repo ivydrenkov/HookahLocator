cfab0b9dd4a75b28881e82e94a1e9b78f1a53c70
ivydrenkov <yfrom5023@gmail.com>
2016-02-24 14:43:31 +0300

    Added yandex metirc

--- app/src/main/java/ru/hookahlocator/hooklocator/HookahLocatorApp.java
+++ app/src/main/java/ru/hookahlocator/hooklocator/HookahLocatorApp.java
@@ -2,6 +2,8 @@ package ru.hookahlocator.hooklocator;
 
 import android.app.Application;
 
+import com.yandex.metrica.YandexMetrica;
+
 import ru.hookahlocator.hooklocator.dagger.Injector;
 
 /**
@@ -13,6 +15,10 @@ public class HookahLocatorApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        YandexMetrica.activate(getApplicationContext(), "11d5368b-39cc-46ee-98e2-1ef05b2c9a49");
+        // Отслеживание активности пользователей
+        YandexMetrica.enableActivityAutoTracking(this);
 
         Injector.init(this);
     }
